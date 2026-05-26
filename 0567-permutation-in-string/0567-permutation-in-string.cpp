4233de38f85e97263e8df45e32a4ec4c6417180e
Vishal Yadav <110190901+VishalYadav6392@users.noreply.github.com>
2026-05-26 17:46:23 +0530

    Time: 0 ms (100%), Space: 10 MB (45.62%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-     ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
-        vector<int> s1hash(26,0);
-        vector<int> s2hash(26,0);
-        int s1len = s1.size();
-        int s2len = s2.size();
-        if(s1len>s2len)
-            return false;
-        
-        int left=0,right=0;
-        while(right<s1len)
+        int n1=s1.size();
+        int n2=s2.size();
+        vector<int>freq1(26,0);
+        vector<int>freq2(26,0);
+       if(n1>n2)return false;
+        for(auto ch :s1)
         {
-            s1hash[s1[right]-'a'] +=1;
-            s2hash[s2[right]-'a'] +=1;
-            right +=1;
+            freq1[ch-'a']++;
         }
-        right -=1;  // to point right to the end of the window
-        while(right<s2len)
+        int right=0,left=0;
+        while(right<n1)
         {
-            if(s1hash==s2hash)
-                return true;
-            right+=1;
-            if(right!=s2len)
-                s2hash[s2[right]-'a'] +=1;
-            s2hash[s2[left]-'a'] -=1;
-            left +=1;
+            freq2[s2[right]-'a']++;
+            right++;
         }
-        return false;   
+        while(right<n2)
+        {
+            if(freq1==freq2)return true;
+            freq2[s2[left]-'a']--;
+            left++;
+            freq2[s2[right]-'a']++;
+            right++;
+        }
+    
+        return freq1==freq2;
+
     }
 };
