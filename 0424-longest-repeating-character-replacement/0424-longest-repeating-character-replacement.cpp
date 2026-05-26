86e2bdf4215b303523ac45b20999333a2442839e
Vishal Yadav <110190901+VishalYadav6392@users.noreply.github.com>
2026-05-26 15:38:48 +0530

    Time: 2 ms (71.32%), Space: 10.8 MB (69.83%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int maxlen=0;
         int n=s.size();
-        int maxfreq=0;
-        int hash[26]={0};
         int l=0,r=0;
+        vector<int>freq(26,0);
+        int ans=0;
+        int maxFreq=0;
         while(r<n)
         {
-            hash[s[r] - 'A']++;  // Increment frequency of character at right pointer
-            maxfreq = max(maxfreq, hash[s[r] - 'A']);  // Update maxfreq
-
-            int change = (r - l + 1) - maxfreq;  // Calculate characters to replace
-
-            if (change > k) {
-                hash[s[l] - 'A']--;  // Remove character at left pointer
-                l++;  // Move left pointer forward
+            freq[s[r]-'A']++;
+            maxFreq=max(maxFreq,freq[s[r]-'A']);
+            while((r - l + 1) - maxFreq > k)
+            {
+                freq[s[l]-'A']--;
+                maxFreq=max(maxFreq,freq[s[l]-'A']);
+                l++;
+                
             }
-            maxlen = max(maxlen, r - l + 1);
+            ans=max(ans,r-l+1);
             r++;
         }
-        return maxlen;
+        return ans;
         
     }
 };
