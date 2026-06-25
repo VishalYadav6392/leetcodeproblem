d9b634bc98468605a955439a7cfcbd6fc26cd25c
Vishal Yadav <110190901+VishalYadav6392@users.noreply.github.com>
2026-06-25 12:56:58 +0530

    Time: 6 ms (93.9%), Space: 41.7 MB (26.36%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -1,23 +1,42 @@
+class FenwickTree {
+    std::vector<int> tree;
+public:
+    FenwickTree(int size) {
+        tree.assign(size + 1, 0);
+    }
+    void update(int i, int delta) {
+        for (; i < tree.size(); i += i & (-i)) tree[i] += delta;
+    }
+    int query(int i) {
+        int sum = 0;
+        for (; i > 0; i -= i & (-i)) sum += tree[i];
+        return sum;
+    }
+};
 class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
-        int n=nums.size();
-        int len=0;
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-             int cnt=0;
-            for(int j=i;j<n;j++)
-            {
-                if(nums[j]==target)
-                {
-                    cnt++;
-                }
-                len=j-i+1;
-                if(2*cnt>len)ans++;
-
+     int n = nums.size();
+        int offset = n + 1;
+        FenwickTree bit(2 * n + 2);
+        
+        long long ans = 0;
+        int current_balance = 0;
+        
+        bit.update(0 + offset, 1);
+        
+        for (int num : nums) {
+            if (num == target) {
+                current_balance += 1;
+            } else {
+                current_balance -= 1;
             }
+            
+            ans += bit.query(current_balance + offset - 1);
+            bit.update(current_balance + offset, 1);
         }
+        
         return ans;
+  
     }
 };
